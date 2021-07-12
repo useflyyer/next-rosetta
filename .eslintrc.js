d633d9cbe1e0b23ef23195ae2cde0bda09880ca8
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-07-12 10:13:59 -0400

    chore: Update deps and rename to Flyyer.io

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
-    "@flayyer/eslint-config",
-    "@flayyer/eslint-config/typescript",
-    "@flayyer/eslint-config/react",
-    // "@flayyer/eslint-config/jest",
-    "@flayyer/eslint-config/prettier",
+    "@flyyer/eslint-config",
+    "@flyyer/eslint-config/typescript",
+    "@flyyer/eslint-config/react",
+    // "@flyyer/eslint-config/jest",
+    "@flyyer/eslint-config/prettier",
   ],
   rules: {
     "react/react-in-jsx-scope": "off",
